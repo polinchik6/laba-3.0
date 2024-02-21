90edaae11829019b25419e0e9ac1866f85210093
polinchik6 <polinawey2@gmail.com>
2024-02-21 12:58:52 +0300

    add7

---
+++ Res2/laba_2.cpp
@@ -0,0 +1,13 @@
+﻿// laba_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	int i;
+	std::cin >> i;
+	unsigned char x = 7;
+	bool n = char(1 * pow(2, i)) & x;
+	std::cout << n << std::endl;
+}
